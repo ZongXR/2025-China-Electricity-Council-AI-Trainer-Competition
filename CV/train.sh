3053e1fc3aaa38573b540874a440b784798de13d
zong.xiangrui <zxr@tju.edu.cn>
2025-11-03 14:38:14 +0800

    目标检测改用ppyoloe+模型
    目标检测改用ppyoloe+模型

--- CV/train.sh
+++ CV/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0 # windows和Mac下不需要执行该命令
-python tools/train.py -c configs/yolo11/yolo11_s_600e_coco.yml --amp --eval --vdl_log_dir=vdl_dir/scalar --use_vdl=true
+python tools/train.py -c configs/ppyoloe/ppyoloe_plus_crn_x_80e_coco.yml --amp --eval --vdl_log_dir=vdl_dir/scalar --use_vdl=true
--- CV/val.sh
+++ CV/val.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
-python tools/eval.py -c configs/yolo11/yolo11_s_600e_coco.yml -o weights=output/yolo11_s_600e_coco/best_model.pdparams --classwise
+python tools/eval.py -c configs/ppyoloe/ppyoloe_plus_crn_x_80e_coco.yml -o weights=output/ppyoloe_plus_crn_x_80e_coco/best_model.pdparams --classwise

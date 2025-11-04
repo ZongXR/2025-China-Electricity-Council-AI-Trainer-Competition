5b7455db1547c00f57dd2740abadc9dbe4c5c821
zong.xiangrui <zxr@tju.edu.cn>
2025-11-04 15:24:50 +0800

    推理脚本新增保存json选项
    推理脚本新增保存json选项

--- CV/infer.sh
+++ CV/infer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
-python tools/infer.py -c configs/ppyoloe/ppyoloe_plus_crn_x_80e_coco.yml -o weights=output/ppyoloe_plus_crn_x_80e_coco/best_model.pdparams --infer_dir=dataset/AutoDrive/test --draw_threshold=0.5
+python tools/infer.py -c configs/ppyoloe/ppyoloe_plus_crn_x_80e_coco.yml -o weights=output/ppyoloe_plus_crn_x_80e_coco/best_model.pdparams --infer_dir=dataset/AutoDrive/test --draw_threshold=0.5  --save_results=true
